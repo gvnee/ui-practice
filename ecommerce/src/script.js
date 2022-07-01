43eab6ffc8abc76f7e9e7452cb23b164a52c620a
gvne <usopp.plazma@yahoo.com>
2022-07-01 20:46:04 +0800

    changes

--- ecommerce/src/script.js
+++ ecommerce/src/script.js
@@ -22,9 +22,13 @@ let previousImage = 0;
 
 for(let i = 0;i<smollImages.length;i++){
     smollImages[i].addEventListener("click", e => {
-        bigImages[previousImage].classList.add("hidden");
+
+        // bigImages[previousImage].classList.add("hidden");
+        // bigImages[i].classList.remove("hidden");
+
+        imageSlide(bigImages, previousImage, i);
+
         previousImage = i;
-        bigImages[i].classList.remove("hidden");
 
         const formerActiveTumbnail = document.getElementById("selectedTumbnail");
         formerActiveTumbnail.removeAttribute("id");
@@ -39,3 +43,39 @@ const cartDropdown = document.getElementById("cart-dropdown");
 cartButton.addEventListener("click", e => {
     cartDropdown.classList.toggle("hidden");
 });
+
+//popup
+const popup = document.querySelector(".popup-preview");
+
+let previuosPopupImage = 0;
+
+for(let i = 0;i<bigImages.length;i++){
+    bigImages[i].addEventListener("click", e => {
+
+        //activate popup
+        popup.classList.remove("hidden");
+        document.body.classList.add("popup-active");
+
+
+
+        const popupImages = document.querySelectorAll(".popup-image");
+        imageSlide(popupImages, previuosPopupImage, i);
+        previuosPopupImage = i;
+        
+    });
+}
+
+//close button
+const closeButton = document.querySelector(".close-button");
+
+closeButton.addEventListener("click", e => {
+    popup.classList.add("hidden");
+    document.body.classList.remove("popup-active");
+});
+
+
+
+function imageSlide(images, current, next){
+    images[current].classList.add("hidden");
+    images[next].classList.remove("hidden");
+}
